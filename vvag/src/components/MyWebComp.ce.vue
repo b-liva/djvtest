c67929fc630001dee66a291574ad47628050bb54
liva <ebammo20@gmail.com>
2022-08-22 00:43:19 +0430

    pass props to custom web elements

--- vvag/src/components/MyWebComp.ce.vue
+++ vvag/src/components/MyWebComp.ce.vue
@@ -1,10 +1,15 @@
 <template>
-  <div><p style="background-color: red">This is a web component</p></div>
+  <div>
+    <p style="background-color: red">This is a web component.</p>
+    <p v-if="cid">cid: {{cid}}</p>
+    <p v-else>No cid</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "MyWebComp"
+  name: "MyWebComp",
+  props: ['cid']
 }
 </script>
 
